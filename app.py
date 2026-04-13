68199d81268ebe071c27e3fc8fcd4c1156d2b92d
M-E-LUNA <me.luna050@gmail.com>
2026-04-13 17:57:15 -0600

    Agregado Navbar, icono de usuario y formulario de registro

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
+import os
+import sys
 from flask import Flask, render_template
 import webbrowser
 from threading import Timer
 
-app = Flask(__name__)
+if getattr(sys, 'frozen', False):
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
+    static_folder = os.path.join(sys._MEIPASS, 'static')
+    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+else:
+    app = Flask(__name__)
 
 @app.route('/')
 def home():
-    
     return render_template('index.html')
 
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
 def open_browser():
-    
     webbrowser.open_new('http://127.0.0.1:5000/')
 
 if __name__ == '__main__':
-    
     Timer(1, open_browser).start()
-
     app.run(port=5000)
